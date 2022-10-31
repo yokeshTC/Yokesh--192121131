f108156c33ac7a9a898a71ce932457e2ef4d0781
yokeshTC <112368654+yokeshTC@users.noreply.github.com>
2022-10-31 12:42:19 +0530

    Add files via upload

---
+++ 192121131--Arithmetic operation.cpp
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+
+{
+    int a,b;
+    int op;
+    printf(" 1.Addition\n 2.Subtraction\n 3.Multiplication\n 4.Division\n 5.Modulus\n 6.exit\n" );
+    printf("Enter the values of a & b: ");
+    scanf("%d %d",&a,&b);
+    printf("Enter your Choice : ");
+    scanf("%d",&op);
+    switch(op)
+    {
+    case 1	:
+        printf("Sum of %d and %d is : %d",a,b,a+b);
+        break;
+    case 2	:
+        printf("Difference of %d and %d is : %d",a,b,a-b);
+        break;
+    case 3	:
+        printf("Multiplication of %d and %d is : %d",a,b,a*b);
+        break;
+    case 4	:
+    	if(b==0)
+    	{
+    		printf("not divisible by zero");
+		}
+		else{
+		
+        printf("Division of  %d and %d is : %d ",a,b,a/b);
+        break;
+    }
+    case 5  :
+    	printf("modulus of %d and %d is : %d ",a,b,a%b);
+    	break;
+    case 6  :
+    	printf("exit");
+    	break;
+    default	:
+        printf(" Enter Your Correct Choice.");
+        break;
+    }
+    return 0;
+}
